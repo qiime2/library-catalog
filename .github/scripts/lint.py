c881cd132c3a5591ce72b021ac3c9f0346c11841
Oddant1 <oddant1@hotmail.com>
2025-01-21 16:24:54 -0700

    github token

--- .github/scripts/lint.py
+++ .github/scripts/lint.py
@@ -10,6 +10,8 @@ FILE_EXT = '.yml'
 
 KEY_SET = set(['owner', 'name', 'branch', 'docs'])
 
+GITHUB_TOKEN = sys.argv[1]
+
 
 def lint(yml):
     # Assert corrrect keys
@@ -21,10 +23,44 @@ def lint(yml):
 
     # Put together the owner/name:branch
     # ...Do something with it
+    base_url = "https://api.github.com"
+
+    def create_repo(access_token, repo_name, repo_descr=None):
+        url = f"{base_url}/user/repos"
+
+        headers = {
+            "Authorization": f"token {access_token}",
+        }
+
+        # create json data to send using the post request
+        data = {
+            "name": repo_name,
+            "description": repo_descr,
+        }
+
+        response = requests.post(url, headers=headers, json=data)
+
+        if response.status_code == 201:
+            repo_data = response.json()
+            return repo_data
+        else:
+            return None
+
+
+    access_token = "YOUR ACCESS TOKEN"
+    repo_name = "apify_testing"
+    repo_descr = "New repo created using the Python GitHub API."
+
+    new_repo = create_repo(access_token, repo_name, repo_descr)
+
+    if new_repo:
+        print(f"New public repo created successfully!")
+    else:
+        print("Failed to create a new repo.")
 
 
 if __name__ == "__main__":
-    files = sys.argv[1:]
+    files = sys.argv[2:]
 
     for file in files:
         head, tail = os.path.split(file)
