ad1154bf0ebf2e791b433b486fdaee8d65dfeccd
Oddant1 <oddant1@hotmail.com>
2025-01-22 12:07:40 -0700

    Get environment-files

--- .github/scripts/lint.py
+++ .github/scripts/lint.py
@@ -11,7 +11,7 @@ FILE_EXT = '.yml'
 KEY_SET = set(['owner', 'name', 'branch', 'docs'])
 
 GITHUB_TOKEN = sys.argv[1]
-
+GITHUB_BASE_URL = 'https://api.github.com'
 
 def lint(yml):
     # Assert corrrect keys
@@ -23,40 +23,20 @@ def lint(yml):
 
     # Put together the owner/name:branch
     # ...Do something with it
-    base_url = "https://api.github.com"
+    url = f'{GITHUB_BASE_URL}/repos/{yml['owner']}/{yml['name']}/contents/environment-files'
+    headers = {
+        'Authorization': f'token: {GITHUB_TOKEN}',
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+    query_params = {
+        'owner': yml['owner'],
+        'repo': yml['name'],
+        'ref': yml['branch'],
+        'path': '/environment-files/'
+    }
 
-    def create_repo(access_token, repo_name, repo_descr=None):
-        url = f"{base_url}/user/repos"
-
-        headers = {
-            "Authorization": f"token {access_token}",
-        }
-
-        # create json data to send using the post request
-        data = {
-            "name": repo_name,
-            "description": repo_descr,
-        }
-
-        response = requests.post(url, headers=headers, json=data)
-
-        if response.status_code == 201:
-            repo_data = response.json()
-            return repo_data
-        else:
-            return None
-
-
-    access_token = "YOUR ACCESS TOKEN"
-    repo_name = "apify_testing"
-    repo_descr = "New repo created using the Python GitHub API."
-
-    new_repo = create_repo(access_token, repo_name, repo_descr)
-
-    if new_repo:
-        print(f"New public repo created successfully!")
-    else:
-        print("Failed to create a new repo.")
+    response = requests.get(url, headers=headers, params=query_params)
+    print(response.json())
 
 
 if __name__ == "__main__":
