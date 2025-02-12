1205469f6fb812f54903670184a1c3660aa5337f
Oddant1 <oddant1@hotmail.com>
2025-02-12 12:29:58 -0700

    Merge

--- .github/scripts/lint.py
+++ .github/scripts/lint.py
@@ -1,9 +1,15 @@
+import os
+import re
 import sys
 import yaml
 import requests
 
 
 KEY_SET = set(['owner', 'name', 'branch', 'docs'])
+
+
+ENV_FILE_REGEX = '.*-qiime2-.*-20[0-9][0-9]\.([1-9]|1[0-2])\.yml'
+env_urls = []
 
 
 def lint(yml):
@@ -16,6 +22,24 @@ def lint(yml):
 
     # Put together the owner/name:branch
     # ...Do something with it
+    url = f'{GITHUB_BASE_URL}/repos/{yml['owner']}/{yml['name']}/contents/environment-files'
+    headers = {
+        'Authorization': f'token: {GITHUB_TOKEN}',
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+    query_params = {
+        'owner': yml['owner'],
+        'repo': yml['name'],
+        'ref': yml['branch'],
+        'path': '/environment-files/'
+    }
+
+    response = requests.get(url, headers=headers, params=query_params)
+    envs = response.json()
+
+    for env in envs:
+        if re.search(ENV_FILE_REGEX, env.name) is not None:
+            env_urls.append(env['download_url'])
 
 
 if __name__ == "__main__":
