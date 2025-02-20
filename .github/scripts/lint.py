c0cab9f0e06fb51d085ffb9838dc32702a952f2e
Oddant1 <oddant1@hotmail.com>
2025-02-20 14:01:38 -0700

    Formatting and comments in lint.py

--- .github/scripts/lint.py
+++ .github/scripts/lint.py
@@ -16,6 +16,7 @@ env_urls = []
 GITHUB_TOKEN = sys.argv[1]
 GITHUB_BASE_URL = 'https://api.github.com'
 
+
 def lint(yml):
     # Assert corrrect keys
     assert set(yml.keys()) == KEY_SET
@@ -25,7 +26,6 @@ def lint(yml):
     assert response.status_code == 200
 
     # Put together the owner/name:branch
-    # ...Do something with it
     url = f'{GITHUB_BASE_URL}/repos/{yml['owner']}/{yml['name']}/contents/environment-files'
     headers = {
         'Authorization': f'token: {GITHUB_TOKEN}',
@@ -38,12 +38,16 @@ def lint(yml):
         'path': '/environment-files/'
     }
 
+    # Get all files in the /environment-files/ folder
     response = requests.get(url, headers=headers, params=query_params)
     envs = response.json()
 
+    # If the file matches the regex to be a QIIME 2 environment-file then keep
+    # track of its download URL
     for env in envs:
         if re.search(ENV_FILE_REGEX, env['name']) is not None:
             env_urls.append(env['download_url'])
+
 
 if __name__ == "__main__":
     GITHUB_TOKEN = sys.argv[1]
