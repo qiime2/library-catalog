dcef836afdb29d29b79ec93de990544fca14f1e1
Oddant1 <oddant1@hotmail.com>
2025-07-10 11:02:09 -0700

    Let's just check for exactly the files we want

--- .github/scripts/errors/lint-errors.js
+++ .github/scripts/errors/lint-errors.js
@@ -1,12 +1,14 @@
 // TODO: If the parser is ever updated this will need to be updated as well
 import yaml from "js-yaml"
 import fs from "fs";
-import path from "path";
 import { get_parser } from "./parser.js";
 
 const PARSER = get_parser();
-const ERROR_DIR = "./library-catalog/errors";
-const ERROR_FILES = fs.readdirSync(ERROR_DIR);
+const ERROR_FILES = [
+    "./library-catalog/errors/LowSeverityErrors.yml",
+    "./library-catalog/errors/MediumSeverityErrors.yml",
+    "./library-catalog/errors/HighSeverityErrors.yml"
+];
 const EXPECTED_KEYS = new Set(["name", "query", "date", "description"]);
 
 function setsEqual(setA, setB) {
@@ -14,14 +16,14 @@ function setsEqual(setA, setB) {
                 [...setA].every(value => setB.has(value));
 }
 
-for (const file of ERROR_FILES) {
+for (const error_file of ERROR_FILES) {
     let loaded_yaml;
 
     // Make sure the file loads as yaml
     try {
-        loaded_yaml = yaml.load(fs.readFileSync(path.join(ERROR_DIR, file), 'utf8'));
+        loaded_yaml = yaml.load(fs.readFileSync(error_file, 'utf8'));
     } catch (error) {
-        throw new Error(`The file '${file}' failed to parse as yaml:\n\n${error.message}`);
+        throw new Error(`The file '${error_file}' failed to parse as yaml:\n\n${error.message}`);
     }
 
     for (const loaded_error of loaded_yaml) {
@@ -29,7 +31,7 @@ for (const file of ERROR_FILES) {
         const found_keys = new Set(Object.keys(loaded_error));
 
         if (!setsEqual(EXPECTED_KEYS, found_keys)) {
-            throw new Error(`The error:\n\n${loaded_error}\n\nFrom the file '${file}'
+            throw new Error(`The error:\n\n${loaded_error}\n\nFrom the file '${error_file}'
                 does not contain the expected keys.\n\nExpected keys are:
                 ${EXPECTED_KEYS}\nKeys found: ${found_keys}`);
         }
@@ -38,7 +40,7 @@ for (const file of ERROR_FILES) {
         try {
             PARSER.parse(loaded_error.query);
         } catch (error) {
-            throw new Error(`The query '${loaded_error.query}' for the error '${loaded_error.name}' from the file: '${file}'
+            throw new Error(`The query '${loaded_error.query}' for the error '${loaded_error.name}' from the file: '${error_file}'
                 failed to parse.\n\n${error.message}`);
         }
     }
